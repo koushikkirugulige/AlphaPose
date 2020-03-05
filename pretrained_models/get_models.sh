9496cbec1481fdd09296640734a2b2d5f6c769c5
Koushik R Kirugulige <koushikkirugulige@gmail.com>
2020-03-05 21:18:56 +0530

    Update get_models.sh

--- pretrained_models/get_models.sh
+++ pretrained_models/get_models.sh
@@ -5,5 +5,5 @@
 
 echo 'Getting the YOLOv3 model'
 echo 'Starting Download...'
-wget --no-check-certificate https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/yolov3.weights
 echo 'Download completed successfully!!'
